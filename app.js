2c28c35c3d8d0ddc31d263816da7633ad5860846
JOGOH MYKAEL <michaeljogoh>
2022-07-29 14:46:37 +0100

    update s3

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
+const { ManagedUpload } = require('aws-sdk/clients/s3');
 const express = require('express');
 const app = express();
 const multer = require('multer');
 require('dotenv').config()
 const PORT = process.env.PORT
-const upload = multer({dest: "/public/uploads"})
+const upload = multer({dest: "/public/uploads"});
+const {uploadFile} = require('./utils/s3')
 
 app.get('/' , (req , res ) =>{
     res.send("Aws-s3 file upload")
 });
 
 
-app.post('/uploads' , upload.single('avatar'), (req , res)=>{
+app.post('/uploads' , upload.single('avatar'), async(req , res)=>{
     const file = req.file
     console.log(file)
+    const result = await uploadFile(file)
+    res.status(200).json({result})
 
 })
 
--- utils/s3.js
+++ utils/s3.js
@@ -1,13 +1,32 @@
-const s3 = require('aws-sdk/client/s3')
+require('dotenv').config()
+const S3 = require('aws-sdk/clients/s3');
+const fs = require('fs')
+
 
 const bucketName  = process.env.AWS_BUCKET_NAME
 const  region = process.env.AWS_BUCKET_REGIONS
 const accessKeyId  = process.env.AWS_BUCKET_ACCESS_KEY
 const  secretAccessKey = process.env.AWS_BUCKET_SECRET_ACCESS_KEY
 
-const s3 = new s3({
-    bucketName,
+const s3 = new S3({
     region, 
     accessKeyId,
     secretAccessKey
 });
+
+
+// upload file
+function uploadFile(file) {
+    const fileStream = fs.createReadStream(file.path)
+    const uploadParams ={
+        Bucket: bucketName,
+        Body:fileStream,
+        Key: file.filename
+
+    }
+    return s3.upload(uploadParams).promise()
+
+
+}
+
+exports.uploadFile = uploadFile
