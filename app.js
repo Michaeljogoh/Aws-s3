b9bd8ee968c731318d5a451da45e2ece1195a2b8
JOGOH MYKAEL <michaeljogoh>
2022-07-29 12:43:00 +0100

    Aws-s3

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const express = require('express')
 const app = express();
 require = ('dotenv').config
 const PORT = process.env.PORT
-const multer = require('multer');
+const multer  = require('multer')
 const upload = multer({dest: "/uploads"})
 
 app.get('/' , (req , res ) =>{
     res.send("Aws-s3 file upload")
 });
+app.post('/' , aws)
 
 app.post('/uploads' ,upload.single('avatar'), (req , res)=>{
     const file = req.file
